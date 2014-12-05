c9da171b33eb6228a5b1c5ed4879939338618784
Shawn O'Hare <shawn@140proof.com>
2014-12-04 16:11:43 -0800

    Update symlinks.sh.

--- symlinks.sh
+++ symlinks.sh
@@ -4,3 +4,4 @@ ln -s ~/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/profile ~/.profile
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/dotfiles/zsh/zsh ~/.zsh
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
