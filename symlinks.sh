1ef8c3b7c7a360b6c823ef7ccaa9bf1db6c46382
Shawn O'Hare <shawn@140proof.com>
2015-04-24 14:49:47 -0700

    Add zshenv file.
    The zshenv file immediately defines ZDOTDIR and exports a path for use
    with the Haskell for OS X app.
    
    Restructured where zsh files live.  Now we create a .zsh dir in the home
    dir and symlink from the dotfiles repo into this.  The .zsh file
    contains everything related to zsh, theoretically, such as the command
    history.

--- symlinks.sh
+++ symlinks.sh
@@ -3,6 +3,9 @@ ln -s ~/dotfiles/aliases ~/.aliases
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/profile ~/.profile
-ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
-ln -s ~/dotfiles/zsh/zsh ~/.zsh
 ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+
+# zsh config files are dotted to limit extraneous symlinks
+mkdir ~/.zsh
+ln -s ~/dotfiles/zsh/zshenv ~/.zshenv
+ln -s ~/dotfiles/zsh/zshrc ~/.zsh/.zshrc
---
+++ zsh/README.md
@@ -0,0 +1,6 @@
+# zsh Config Notes
+
+- Make $HOME/.zsh directory
+- Symlink zshenv to $HOME/.zshenv.
+- zshenv sets ZDOTDIR to  $HOME/.zsh
+- Symlink dotted versions of config files (e.g., zshrc) into $HOME/$ZDOTDIR/
