4b75ffeb3c6312381db22c6702c12ebf023cf88e
Shawn O'Hare <ohare.shawn@gmail.com>
2016-06-12 19:41:19 -0700

    [emacs] Begin to utilize use-package

--- emacs/.emacs.d/customize.el
+++ emacs/.emacs.d/customize.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (misterioso))))
+ '(package-selected-packages
+   (quote
+    (smex evil-mode evil-leader zenburn-theme monokai-theme helm gruvbox-theme evil-commentary atom-one-dark-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,13 +1,98 @@
 (require 'package)
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
-; Define file the customize UI writes to.
+;; Define file the customize UI writes to.
 (setq custom-file "~/.emacs.d/customize.el")
 (load custom-file)
+
+;; Prefer spaces to tabs.
+(setq-default indent-tabs-mode nil)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t)
+(defun my-move-key (keymap-from keymap-to key)
+  "Moves key binding from one keymap to another, deleting from the old location. "
+  (define-key keymap-to key (lookup-key keymap-from key))
+  (define-key keymap-from key nil))
+
+
+(use-package helm)
+
+(use-package ido
+  :init (progn (ido-mode 1)
+	       (ido-everywhere))
+  :config
+  (progn
+    (setq ido-enable-flex-matching t)))
+
+(use-package evil
+  :init
+  (progn
+    ;; if we don't have this evil overwrites the cursor color
+    (setq evil-default-cursor t)
+
+    (use-package evil-commentary
+      :config
+      (evil-commentary-mode))
+
+    ;; leader shortcuts
+    ;; This has to be before we invoke evil-mode due to:
+    ;; https://github.com/cofi/evil-leader/issues/10
+    (use-package evil-leader
+      :init (global-evil-leader-mode)
+      :config
+      (progn
+        (setq evil-leader/in-all-states t)
+        (evil-leader/set-leader "<SPC>")
+        (evil-leader/set-key
+          "b" 'ido-switch-buffer
+          "f" 'ido-find-file
+          "i" 'idomenu
+          "k" 'kill-buffer
+          "K" 'kill-this-buffer
+          "o" 'occur
+          "T" 'eshell
+          "wd" 'delete-window
+          "wo" 'delete-other-windows
+          "ws" 'split-window-below
+          "wh" 'split-window-horizontally
+          "wv" 'split-window-vertically
+          "ww" 'other-window
+          "x" 'smex)))
+
+    ;; boot evil by default
+    (evil-mode 1))
+  :config
+  (progn
+    ;; Enter ex-mode using smex
+    (define-key evil-ex-map "e " 'ido-find-file)
+    (define-key evil-ex-map "b " 'ido-switch-buffer)
+    (define-key evil-ex-map "x" 'smex)
+
+    ;; recover emacs key bindings in motion state
+    (my-move-key evil-motion-state-map evil-normal-state-map (kbd "RET"))
+    (my-move-key evil-motion-state-map evil-normal-state-map " ")
+
+    ;; esc should always quit: http://stackoverflow.com/a/10166400/61435
+    (define-key evil-normal-state-map [escape] 'keyboard-quit)
+    (define-key evil-visual-state-map [escape] 'keyboard-quit)
+    (define-key minibuffer-local-map [escape] 'abort-recursive-edit)
+    (define-key minibuffer-local-ns-map [escape] 'abort-recursive-edit)
+    (define-key minibuffer-local-completion-map [escape] 'abort-recursive-edit)
+    (define-key minibuffer-local-must-match-map [escape] 'abort-recursive-edit)
+    (define-key minibuffer-local-isearch-map [escape] 'abort-recursive-edit)))
+
+(use-package smex
+  :defer t
+  :bind (("M-x" . smex)
+         ("M-X" . smex-major-mode-commands))
+  :config
+  (progn
+    (smex-initialize)))
