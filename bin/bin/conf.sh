2f5798b15d0505bd91581542dc46ca0c80f04869
Shawn O'Hare <ohare.shawn@gmail.com>
2016-07-31 16:49:21 -0700

    Update config script to be more generic.

--- bin/bin/conf.sh
+++ bin/bin/conf.sh
@@ -6,6 +6,9 @@
 #    the XDG_DATA_HOME location?
 # 2. Consider putting the hist file in ~/var/zsh/history or something
 
+# FIXME 2016-07-31T13:52:48-0700 
+# - Remove update path logic to keep it simple.
+
 # ---------------------------------------------------------------------------
 # Global Constants 
 # These will be set to the value of
@@ -25,7 +28,11 @@ readonly DOTFILES_REPO="${DOTFILES_REPO:-https://github.com/shawnohare/dotfiles.
 readonly DOTFILES_DEPS="${DOTFILES_DEPS:-${HOME}/opt}"
 readonly LOGFILE="${LOGFILE:-${HOME}/var/log/dotfiles/conf.log}"
 readonly DOTFILES="${DOTFILES:-${HOME}/${DOTFILES_NAME}}"
+# FIXME brewfile is deprecated
 readonly BREWFILE="${BREWFILE:-${DOTFILES}/homebrew/.config/homebrew/Brewfile}"
+
+# ensure the path includes local binaries in case we are in an odd state
+PATH="${HOME}/.nix-profile/bin:${HOME}/bin:/usr/local/bin:${PATH}"
 
 
 # ---------------------------------------------------------------------------
@@ -40,7 +47,6 @@ declare logging=false
 # # ---------------------------------------------------------------------------
 # # General helper funcs. 
 # # ---------------------------------------------------------------------------
-
 
 # Print a time-stamped message.
 # When the --debug or --verbose flags are passed, the statement will
@@ -94,15 +100,73 @@ hash_exists() {
 # When the hash bang is /bin/sh, the exit codes of hash and type are not
 # well-defined.
 posix_exists() {
-  command -v "$1" foo >/dev/null 2>&1
+  command -v "$1" >/dev/null 2>&1
 }
 
+# tests whether a package has been installed by nix
+is_pkg_installed() {
+  nix-env -q "$1" >/dev/null 2>&1;
+} 
+
+
+# require a command to exist.  Exit with an error if it does not.
+require() {
+  local cmd=$1
+  if ! posix_exists "${cmd}"; then 
+    echo --error "${cmd} required to install configuration. Exiting"
+    exit 1
+  fi
+  return 0
+}
+
+
+
+# idempotently install the nix package manager.
+get_nix() {
+  if ! posix_exists "nix-env"; then
+    echo "Installing the nix package manager."
+    $dry || curl "https://nixos.org/nix/install" | sh
+  fi
+}
+
+
+# get a pkg using nix if it does not already exist
+get_pkg() {
+  local pkg=$1
+  if ! is_pkg_installed ${pkg}; then
+    echo "Installing ${pkg}."
+    $dry || nix-env --install "${pkg}"
+  else
+    echo --debug "Package ${pkg} already installed."
+  fi
+}
+
+# ensure a command exists.  If it doesn't, install with nix.  If it does
+# exist but is not managed by nix, prompt the user if they want to install
+# with nix.
+ensure() {
+  local cmd=$1
+
+  if ! posix_exists ${cmd}; then
+    get_pkg "${cmd}"
+    return 0
+  fi
+
+  # cmd exists, but it's not installed by nix.
+  if ! is_pkg_installed ${cmd}; then
+    printf "[${0}] $(date "+%Y-%m-%d %H:%M:%S")  A version of ${cmd} exists outside of nix.  Install with nix? (y/n): " 
+    read a
+    [ $a = "y" ] && get_pkg "${cmd}"
+  fi
+  return 0
+}
 # ---------------------------------------------------------------------------
 # Inititialization 
 # These functions are rather fine-grained so that they are easier to debug.
 # ---------------------------------------------------------------------------
 
 # Update the path if necessary. Useful when testing whether a bin exists.
+# FIXME: deprecated
 update_path() {
   if [[ ! "${PATH}" =~ "usr.local" ]]; then
     echo --debug "Appending /usr/local to PATH."
@@ -163,15 +227,17 @@ get_git_repo() {
   echo --debug "Checking for git repo in ${loc}"
   if [[ ! -d "${loc}" ]]; then
     echo --debug "Cloning ${remote} to ${loc}"
-    mkdir -p "${loc}"
-    git clone --recursive "${remote}" "${loc}"
+    if ! $dry; then
+      mkdir -p "${loc}"
+      git clone --recursive "${remote}" "${loc}"
+    fi
     return 0
   else
-    echo --debug "Dir ${loc} already exists."
+    echo --debug "Git repo in ${loc} exists."
   fi
 
   if $update; then
-    echo --debug "Updating ${remote} in ${loc}  "
+    echo --debug "Updating git repo in ${loc}  "
     cd "${loc}"
 
     # Check whether there are changes.
@@ -184,6 +250,8 @@ get_git_repo() {
       echo --debug "There are staged, uncomitted changes in ${loc}.  Not updating repo."
       return 1
     fi
+
+    $dry || git pull
     echo --debug "Updated git repo in ${loc}"
   fi
 }
@@ -318,44 +386,38 @@ install_git() {
 
 setup_zsh() {
   echo "Setting up zsh."
-  install "zsh"
   local zsh_path="$(which zsh)"
   echo --debug "zsh path: ${zsh_path}"
 
   # Append Homebrewed zsh path to /etc/shells to authorize it as a login shell
   if [[ -z $(grep "${zsh_path}" /etc/shells) ]]; then 
     echo "Adding ${zsh_path} to /etc/shells."
-    printf "${zsh_path}" | sudo tee -a /etc/shells
+    $dry || printf "${zsh_path}" | sudo tee -a /etc/shells
   fi
 
   # Change this user's default shell to Homebrewed zsh
   if [[ ${SHELL} != "${zsh_path}" ]]; then
     echo "Changing this user's default shell to zsh."
-    chsh -s "${zsh_path}" "${USER}"
+    $dry || chsh -s "${zsh_path}" "${USER}"
   fi
   return 0
 }
 
 
 # Get external dependencies that are not otherwise managed.
-get_deps() {
-  if $dry; then
-    echo "Dry run.  Not getting external repos."
-    return 0
-  fi
-
-
+get_config_deps() {
   get_github_dep "zsh-users/zsh-history-substring-search"
   get_github_dep "zsh-users/zsh-syntax-highlighting"
   get_github_dep "zsh-users/zsh-completions"
   get_github_dep "zsh-users/zsh-autosuggestions"
   get_github_dep "powerline/fonts"
   # Install powerline fonts.
-  bash "${XDG_DATA_HOME}/powerline/fonts/install.sh"
+  $dry || bash "${DOTFILES_DEPS}/powerline/fonts/install.sh"
 }
 
 
 # OS independent package installation function.  Skip if in quick mode.
+# FIXME deprecated
 install_all() {
   if $dry; then
     echo --debug "In drymode.  Skipping package installation."
@@ -375,31 +437,37 @@ Usage: dotfiles.sh [-hvq] [-a a_arg] [cmd]
 Arguments:
 
 -a arg : do something with a_arg
+-d : Execute a dry-run.  The system state is not changed.
 -h : print this help message.
--l : echo to the echofile specified in the LOGFILE env variable.
+-l : echo to the logfile specified in the LOGFILE env variable.
 -v : run in verbose mode to print debugging statements.
--q : run in quick mode.  No packages are installed. Deprecated.  Use commands.
 
 Commands:
 
+init
+
+  Put the system in a state where the rest of the configuration
+  commands will work.  This will create the necessary directories
+  and install external tools needed to get packages and link configs.
+
+
 install 
 
-Does everything.
+  Fully install the dotfiles.  This is equivalent to calling the subcommands
+  init followed by link.
 
 
 link [--all|-a] [...]
 
-Link all dotfiles.  This will copy the directory substructure of each
-dotfiles dir and then use GNU stow to symlink the contents.  The --all flag
-is not necessary, and provided for semantic purposes.  Additional arguments
-for the stow command are passed through.  For example
-  link --restow : calls stow dir --restow
-
-
+  Link all dotfiles.  This will copy the directory substructure of each
+  dotfiles dir and then use GNU stow to symlink the contents.  The --all flag
+  is not necessary, and provided for semantic purposes.  Additional arguments
+  for the stow command are passed through.  For example
+    link --restow : calls stow dir --restow
 
 link --dir|-d dir [...]
 
-Link a single directory, using the same logic as with the link command.
+  Link a single directory, using the same logic as with the link command.
 
 EOF
 }
@@ -415,7 +483,7 @@ parse_opts() {
         ;;
       d)
         dry=true
-        echo "Dry run.  No packages will be installed."
+        echo "Dry run. State will not change." 
         ;;
       h)
         # print help message
@@ -455,12 +523,21 @@ link_dir() {
   echo "Linking ${dir}"
   shift
   cd "${DOTFILES}/${dir}"
-  find . -type d -exec mkdir -p -- ${HOME}/{} \;
+
+  # copy dir struct
+  $dry || find . -type d -exec mkdir -p -- ${HOME}/{} \;
   cd "${DOTFILES}"
   # call stow with the link_dir options
-  stow --verbose "${@}" --target="${HOME}" "${dir}" 
+  $dry || stow --verbose "${@}" --target="${HOME}" "${dir}" 
 }
 
+
+#------------------------------------------------------------------------- 
+# commands
+# Commands are responsible that required packages are installed.
+# This leads to some minor duplication, but means that the checking
+# occurs in a common location.
+#------------------------------------------------------------------------- 
 # link each folder in the dotfiles root to the home dir.
 cmd_link() {
   cd "${DOTFILES}"
@@ -499,52 +576,80 @@ cmd_link() {
 }
 
 # get external dependencies such as zsh modules
+# FIXME for testing purposes really
 cmd_deps() {
-  get_deps
+  ensure "git"
+  get_config_deps
   "${ostype}_teardown"
+}
+
+# init puts things in a state where the other commands work 
+cmd_init() {
+  echo "Initializing."
+  require "curl"
+
+  cd ${HOME}
+
+  # make dirs
+  if ! $dry; then
+    if [ ! -d "/nix" ]; then
+      # wrapped in the conditional so we don't always prompt for password
+      sudo mkdir /nix 
+    fi
+    mkdir -p "${XDG_CONFIG_HOME}"
+    mkdir -p "${XDG_DATA_HOME}"
+    mkdir -p "${XDG_CACHE_HOME}"
+    mkdir -p "${XDG_STATE_HOME}"
+    mkdir -p "${HOME}/bin"
+    mkdir -p "${HOME}/var/log"
+    mkdir -p "${HOME}/tmp/"
+    mkdir -p "${HOME}/opt/"
+  fi
+
+  # get a minimal set of tools required for the config script
+  get_nix
+  get_pkg "git"
+  get_pkg "stow"
+  get_pkg "zsh"
+
+  ensure "git"
+  get_config_deps
+
+  ensure "zsh"
+  setup_zsh
 }
 
 cmd_install() {
   echo "Installing dotfiles."
-  cmd_deps
-  setup_zsh
-  # TODO manually install these big packages perhaps
-  # install_pkgs
+  cmd_init
   cmd_link
-  echo "Finished."
+  echo "Finished installing."
 }
 
 # FIXME for testing purposes
 cmd_zsh() {
+  ensure "zsh"
   setup_zsh
   exit 0
 }
 
-# makes dirs, sets the shell, etc.
-cmd_init() {
-  echo "Initializing."
-  cd ${HOME}
-  mkdir -p "${XDG_CONFIG_HOME}"
-  mkdir -p "${XDG_DATA_HOME}"
-  mkdir -p "${XDG_CACHE_HOME}"
-  mkdir -p "${XDG_STATE_HOME}"
-  mkdir -p "${HOME}/bin"
-  mkdir -p "${HOME}/var/log"
-  mkdir -p "${HOME}/tmp/"
-  mkdir -p "${HOME}/opt/"
-  install_git
-  install "stow" # symlink farm
-  get_git_repo "${DOTFILES_REPO}" "${DOTFILES}" # fetch the dotfiles repo
-  setup_zsh
-}
 
 parse_cmd() {
   local cmd="${1}"
+  local exp=false
   case "${cmd}" in
-    deps | init | install | link | zsh )
+    init | install | link)
       # Execute the named command and pass it the args that follow.
       # For example, "cmd_${@}" could expand to cmd_foo arg1 arg2 arg3.
       "cmd_${@}"
+      ;;
+    --experimental | -e)
+      test=true
+      shift
+      ;;
+    deps | zsh)
+      # only run if preceeded by --experimental
+      $exp && "cmd_${@}"
       ;;
     *)
       echo "Command ${cmd} not recognized".
@@ -554,14 +659,11 @@ parse_cmd() {
   esac
 }
 
-
-
 main() {
   # init
   parse_opts "${@}"
   shift $((OPTIND - 1)) # shift past options to sub-commands 
-  set_ostype
-  update_path
+  set_ostype # FIXME: do we care?
   "${ostype}_init"
   echo --debug "Input commands: ${@}"
 
