f40c47f3ab0e24dc2ce676a700cb9f7a5385a9e5
Shawn O'Hare <ohare.shawn@gmail.com>
2016-08-14 21:25:00 -0700

    Echo change.

--- bin/bin/lib/posix/echo.sh
+++ bin/bin/lib/posix/echo.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 echo() {
-  # prefix="[${0}] $(date "+%Y-%m-%d %H:%M:%S")"
+  # echo_prefix="[${0}] $(date "+%Y-%m-%d %H:%M:%S")"
+  echo_prefix="[${0}]"
 
   while true; do
     case $1 in
       "--error" | "-e")
         shift
-        >&2 printf "%s\n" "${0} Error: ${*}"
+        >&2 printf "%s\n" "${echo_prefix} Error: ${*}"
         return 0
         ;;
       "--prompt" | "-p")
         shift
-        printf "%s: " "${0} ${*}"
+        printf "%s: " "${echo_prefix} Prompt: ${*}"
         return 0
         ;;
       *)
-        printf "%s\n" "${0} ${*}"
+        printf "%s\n" "${echo_prefix} ${*}"
         return 0
         ;;
     esac
