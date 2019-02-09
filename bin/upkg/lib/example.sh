b9a1df0d4f8e3d08772d32fb08c421b4fff893b3
Shawn M. O'Hare <shawn@shawnohare.com>
2019-02-09 10:57:03 -0800

    [wip] Initial interface for upkg.

---
+++ bin/upkg/lib/example.sh
@@ -0,0 +1,17 @@
+get() {
+    echo "get"
+    libupkg::hello 'I am in the example get func' 1 2 3
+}
+
+install() {
+    echo "install"
+}
+
+upgrade() {
+    echo "upgrade"
+
+}
+
+remove() {
+    echo "remove"
+}
